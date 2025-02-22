2b85d972876bad282001f1b3f7f8271d4c6c3469
rudine <rudine.hartanto@intellisense.io>
2025-02-22 23:33:14 +0000

    update interface

--- src/services/applications.service.ts
+++ src/services/applications.service.ts
@@ -1,6 +1,7 @@
 import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FilterQuery, Model, Types } from 'mongoose';
+import { IApplication } from 'src/interfaces/application.interface';
 
 @Injectable()
 export class ApplicationsService {
